8a9f414d8dbb72bf45f21bfa69e210325db3ab61
Guilherme99 <italoguilherme18.1@gmail.com>
2021-09-09 23:26:00 -0300

    routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -28,7 +28,7 @@ const Route: React.FC<RouteProps> = ({
         ) : (
           <Redirect
             to={{
-              pathname: isPrivate ? '/' : '/Home',
+              pathname: isPrivate   ' : '/Home',
               state: { from: location },
             }}
           />
